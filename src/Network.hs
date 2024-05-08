55b5419ef553143496d2f87539a5ee0072bf963b
Gustavo Bauke <baukegustavo@gmail.com>
2024-05-08 01:10:30 -0300

    WIP: creates network file

---
+++ src/Network.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NoStarIsType #-}
+
+module Network where
+
+import Data.Kind
+import GHC.TypeLits
+import Matrix
+
+type Layer :: Nat -> Nat -> Type -> Type
+data Layer r c a where
+  L :: Matrix r c a -> Layer r c a
+
+type Network :: Nat -> [Nat] -> Nat -> Type -> Type
+data Network in' ms out a
